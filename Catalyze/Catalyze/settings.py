ead4346aa4da686b3674f12ec91894ef62a795bf
Neha Vardhinedi <nehavardhinedi@gmail.com>
2017-08-01 14:52:51 -0700

    Changed timezone and username

--- Catalyze/Catalyze/settings.py
+++ Catalyze/Catalyze/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qpxgk+sp)xq790vjpb_bh-uxm*5e#%5jdp1o+&hfd%%j@k^=g7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'Catalyze.pythonanywhere.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
