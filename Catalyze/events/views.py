62fba185db0c8e2f188f0685bf2805fb86aa68cf
jpan <catalyzejpan@gmail.com>
2017-08-02 19:51:50 -0700

    Added view and template for detailed post as well as CSS for site, but CSS is acting weird

--- Catalyze/events/views.py
+++ Catalyze/events/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Event
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
     posts = Event.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'events/post_list.html', {'posts' : posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Event, pk=pk)
+    return render(request, 'events/post_detail.html', {'post': post})
