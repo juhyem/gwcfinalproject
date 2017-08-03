34373bc2d375e077d7e843434870e64e3c2a1919
jpan <catalyzejpan@gmail.com>
2017-08-03 11:14:49 -0700

    Edited views for events

--- Catalyze/events/views.py
+++ Catalyze/events/views.py
@@ -39,4 +39,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = EventForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'events/post_edit.html', {'form': form})
