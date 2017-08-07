8f3414f7777e9813a2ef9d2e5d08d5d80a35f4b8
JuHye <bluemoonjm@gmail.com>
2017-08-07 02:49:22 -0700

    I apparently edited the WRONG INDEXSTYLE I HATE MY LIFE. JuHye should transfer those edits to the RIGHT ONE oh my god this is literally KILLING ME RN. Good news is I edited Anna's image upload to File Field, and I now know where the imgs are going. I created an auto img url maker, and the images for to their post's # toogit status How awesome. :) But the images do not show on postlist/edit yet, so I should do that. Also change File Field to ImageField eventually

--- Catalyze/Catalyze/settings.py
+++ Catalyze/Catalyze/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = "/home/pi/djcode/xpcpro/xpcpro/images/"
-MEDIA_URL = "/images/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- Catalyze/events/urls.py
+++ Catalyze/events/urls.py
@@ -10,9 +10,17 @@ urlpatterns = [
 ]
 
 from django.conf.urls.static import static
+
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL,
-document_root=settings.MEDIA_ROOT)
+# from django.cof import settings
+# from django.conf.urls.static import static
+
+# urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += static(settings.MEDIA_URL,
+# document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
